456b9e012de01528fc81836c703c7cf4353f99d8
Gary Griffaw <ggriffaw@yahoo.com>
2023-05-25 16:06:59 -0500

    Add testGetBeerById

--- src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6reactive.controllers;
 
+import guru.springframework.spring6reactive.model.BeerDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -22,5 +23,14 @@ class BeerControllerTest {
                 .expectStatus().isOk()
                 .expectHeader().valueEquals("Content-type", "application/json")
                 .expectBody().jsonPath("$.size()").isEqualTo(3);
+    }
+
+    @Test
+    void testGetBeerById() {
+        webTestClient.get().uri(BeerController.BEER_PATH_ID, 1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody(BeerDTO.class);
     }
 }
