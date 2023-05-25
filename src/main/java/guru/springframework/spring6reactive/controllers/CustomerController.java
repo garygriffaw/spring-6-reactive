4114d98ca8e769d4c2757a17f035544fa4eae9d6
Gary Griffaw <ggriffaw@yahoo.com>
2023-05-25 14:41:16 -0500

    Add getCustomerById

--- src/main/java/guru/springframework/spring6reactive/controllers/CustomerController.java
+++ src/main/java/guru/springframework/spring6reactive/controllers/CustomerController.java
@@ -4,18 +4,26 @@ import guru.springframework.spring6reactive.model.CustomerDTO;
 import guru.springframework.spring6reactive.services.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
 public class CustomerController {
     public static final String CUSTOMER_PATH = "/api/v2/customer";
+    public static final String CUSTOMER_PATH_ID = CUSTOMER_PATH + "/{customerId}";
 
     private final CustomerService customerService;
 
     @GetMapping(CUSTOMER_PATH)
     Flux<CustomerDTO> listCustomers() {
         return customerService.listCustomers();
+    }
+
+    @GetMapping(CUSTOMER_PATH_ID)
+    Mono<CustomerDTO> getCustomerById(@PathVariable("customerId") Integer customerId) {
+        return customerService.getCustomerById(customerId);
     }
 }
--- src/main/java/guru/springframework/spring6reactive/services/CustomerService.java
+++ src/main/java/guru/springframework/spring6reactive/services/CustomerService.java
@@ -2,7 +2,10 @@ package guru.springframework.spring6reactive.services;
 
 import guru.springframework.spring6reactive.model.CustomerDTO;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface CustomerService {
     Flux<CustomerDTO> listCustomers();
+
+    Mono<CustomerDTO> getCustomerById(Integer customerId);
 }
