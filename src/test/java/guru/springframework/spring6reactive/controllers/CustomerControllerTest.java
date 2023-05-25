573e9d093b99a7decd4347919d8fb5ce8bdd8b58
Gary Griffaw <ggriffaw@yahoo.com>
2023-05-25 16:44:18 -0500

    Add testListCustomers

---
+++ src/test/java/guru/springframework/spring6reactive/controllers/CustomerControllerTest.java
@@ -0,0 +1,30 @@
+package guru.springframework.spring6reactive.controllers;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+class CustomerControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    @Order(1)
+    void testListCustomers() {
+        webTestClient.get()
+                .uri(CustomerController.CUSTOMER_PATH)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody().jsonPath("$.size()").isEqualTo(4);
+    }
+}
