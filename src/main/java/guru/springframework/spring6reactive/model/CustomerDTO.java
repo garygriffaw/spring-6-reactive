5674ee5e8263ef5848783fdb7510f098678ae146
Gary Griffaw <ggriffaw@yahoo.com>
2023-05-25 13:45:36 -0500

    Add Customer entity and dto

---
+++ src/main/java/guru/springframework/spring6reactive/model/CustomerDTO.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring6reactive.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomerDTO {
+    private Integer id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
+    private String name;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
